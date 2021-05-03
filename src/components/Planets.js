23ddcb09c185ba45068e4fd9d34fd41893d19e59
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-05-03 10:22:30 +0200

    Add fetch API

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,18 +1,36 @@
-import { useState } from "react"
-import Planet from "./Planet"
+import { useState, useEffect } from "react";
+import Planet from "./Planet";
 
 const Planets = () => {
-  const [planets, setPlanets] = useState([])
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    fetch("https://swapi.dev/api/planets/?page=1")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `Nous n'avons pas pu lire les registres des planètes, status : ${response.status}`
+          );
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setPlanets(data.results);
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  }, []);
 
   return (
     <>
       <div className="row">
         {planets.map((planet) => {
-          return <Planet key={planet.name} planet={planet} />
+          return <Planet key={planet.name} planet={planet} />;
         })}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Planets
+export default Planets;
